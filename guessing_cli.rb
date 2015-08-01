6d9231c0b0599ef35807f36145362eeda8062439
rolandobrown <rolando.brown@gmail.com>
2015-07-31 20:41:33 -0400

    follow hints build guessing game

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,26 @@
-# Code your solution here!
+def run_guessing_game
+  response = ""
+  while response
+    puts "Guess a number between 1 and 6."
+    response = gets.chomp
+    rand_number = rand(1..6).to_s
+    case response.chomp
+    when rand_number
+      puts "You guessed the correct number!"
+    when 'exit'
+      puts "Goodbye!"
+      break
+    else
+      puts "The computer guessed #{rand_number}."
+    end
+  end
+end
+
+#HINTS
+
+# validate my assumptions
+
+# Hint section in documentation said
+# - should start with __while__
+# - then include __rand__
+# - then __break__
